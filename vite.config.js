a3b15d21a987c84bf2c4973689f0d46d667df8e4
ayanadankhan <ayanadankhan@gmail.com>
2024-11-27 12:57:21 +0500

    fixwd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio/',  // Make sure this matches your GitHub repository name
   build: {
     outDir: 'dist',
   },
