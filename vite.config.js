48c6395fc1182a69443ca7672be1b051c079ff9c
ayanadankhan <ayanadankhan@gmail.com>
2024-11-27 13:01:47 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // Make sure this matches your GitHub repository name
+  base: '/portfolio/',  // This must match your GitHub repository name
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Ensures build files go to the 'dist' folder
   },
 });
